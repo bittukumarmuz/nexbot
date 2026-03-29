1abebddfc6cbda51e7bf1a19260feb51b204c5a0
bittukumarmuz <bittukumarkathaiya@gmail.com>
2026-03-29 10:04:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def status():
 if __name__ == "__main__":
     print("\nNexBot starting...")
     print("Open: http://127.0.0.1:5000\n")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
